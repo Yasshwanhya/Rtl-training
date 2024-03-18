aa6d9bf01492bef2ac433281c0d709d644b2a3d3
Yasshwanhya <128277210+Yasshwanhya@users.noreply.github.com>
2024-03-18 12:33:24 +0530

    Update assignment1.v

--- assignment1.v
+++ assignment1.v
@@ -1,77 +1,64 @@
-module axi_8bit_reg (
-    input              clk,
-    input              reset,
-    
-    // Master input
-    input      [7:0]   m_data_in,
-    input              m_valid,
-    output  reg      m_ready,
-    input              m_last,
-    
-    // Slave output
-    output  reg [7:0]   s_data_out,
-    output   reg          s_valid,
-    input              s_ready,
-    output  reg      s_last
-);
+`timescale 1ns / 1ps
 
-     //Internal registers
-    reg [7:0] data_out;
-    reg        valid;
-    reg ready;
-    reg last;
+module axi_8bit(
+    input clk,rst,
     
-    // AXI stream register logic
-    always @(negedge clk) begin
-        if (reset) begin
-            data_out <= 8'b0;
-            valid <= 1'b0;
-            last <= 1'b0;
-        end else begin
-            if (m_valid && s_ready) begin
-               data_out <= m_data_in;
-              valid  <= 1'b1;
-              ready<=s_ready;
-              last<=m_last;
-            end else 
-            begin
-           if (m_valid)
-           begin
-               data_out<= data_out;
-                valid <= 1'b1;
-              ready<=s_ready;
-              last <= m_last;
-              end
-              else
-              begin
-              data_out<= data_out;
-                valid <= 1'b0;
-              ready<=s_ready;
-              last<=m_last;
-              end
-        end
+    //slave
+    input [7:0]s_data,
+    input s_valid,
+    output reg s_ready,
+    input  s_last,
+    
+    //master
+    output reg [7:0]m_data,
+    output reg m_valid,
+    input m_ready,
+    output  m_last
+     );
+reg [7:0]data;
+reg valid;
+reg ready;
+reg last;
+
+always@(posedge clk) begin
+    valid <= s_valid;
+    ready <= m_ready;
+
+end
+
+always@(negedge clk or negedge rst) begin
+    if(rst) begin
+        data <= 0;
+        valid <= 0;
+        ready <= 0;
+//        last <=0;   
+    end
+    else begin
+     if(s_valid && m_ready) data <= s_data;
+     else data <=0;
     end
 end
- 
-always @(negedge clk) 
- begin
-if(reset)
- s_data_out <= 8'h0;
-  else
-  begin  
-            s_data_out <= data_out;
-            s_valid <= valid;
-            m_ready<=ready;
-            s_last<= m_last;
-     end   
-        
- end   
- 
-    // Output assignment
-//  always @(negedge clk)
-//  begin
-//   s_last <= m_last;
-//  end 
-    
 
+always @(negedge clk)
+begin
+    if(rst) 
+    begin
+        m_data <= 0;
+        m_valid <= 0;
+        s_ready <= 0;
+//        m_last <=0;   
+    end
+    else
+    begin
+       s_ready <= ready;
+       m_data <= data;
+//       m_last <= last;
+       m_valid <= valid;
+    end
+end
+always@(negedge clk)
+begin
+    last <= s_last;
+    end
+assign m_last = last;
 endmodule
